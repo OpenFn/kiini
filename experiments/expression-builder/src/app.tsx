963798b4dfb7bbb7bf6bbc11c57adb4ce2cb009a
Stuart Corbishley <corbish@gmail.com>
2022-01-13 16:32:42 +0200

    Render static code sample in list

--- experiments/expression-builder/src/app.tsx
+++ experiments/expression-builder/src/app.tsx
@@ -1,22 +1,57 @@
 import React, { useCallback, useState } from "react";
 import ReactDOM from "react-dom";
-import "./parser";
+import { generateTree, stringToSourceFile } from "./parser";
 
 const App = (props: { message: string }) => {
-    const [count, setCount] = useState(0);
-    const increment = useCallback(() => {
-        setCount(count => count + 1);
-    }, [count]);
-    return(<>
-        <h1>{props.message}</h1>
-        <h2>Count: {count}</h2>
-        <button onClick={increment}>Increment</button>
-    </>)
+  const [count, setCount] = useState(0);
+  const increment = useCallback(() => {
+    setCount((count) => count + 1);
+  }, [count]);
+  return (
+    <>
+      <h1>{props.message}</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>Increment</button>
+    </>
+  );
 };
 
+const Tree = ({ tree: [parent, children] }: { tree: [string, string[]] }) => {
+  switch (parent) {
+    case "IfStatement":
+      const [condition, statement] = children;
+      return (
+        <li>
+          <div>
+            <h3>{parent}</h3>
+            Condition: <code>{condition}</code><br/>
+            Statement: <Tree tree={statement} />
+          </div>
+        </li>
+      );
+    default:
+      return (
+        <li>
+          {parent}
+          <ul>
+            {children.map((c) => (
+              <Tree tree={c} />
+            ))}
+          </ul>
+        </li>
+      );
+  }
+};
 
+const code = "if(true){alert('foo')}";
+const tree: string[][] = generateTree(stringToSourceFile(code))[0];
 
 ReactDOM.render(
-  <App message="Hello World! Simple Counter App built on ESBuild + React + Typescript"/>,
-  document.getElementById('root')  
+  <>
+    <textarea id="story" name="story" rows="5" cols="33">
+      {code}
+    </textarea>
+    <Tree tree={tree} />
+  </>,
+  document.getElementById("root")
 );
--- experiments/expression-builder/src/parser.ts
+++ experiments/expression-builder/src/parser.ts
@@ -1,14 +1,39 @@
-import * as ts from 'typescript';
+import * as ts from "typescript";
 
 const code = "if(true){alert('foo')}";
 
-const sourceFile = ts.createSourceFile('temp.ts', code);
+const sourceFile = ts.createSourceFile("temp.ts", code, 8);
 let indent = 0;
 
-function printTree(node) {
-    console.log(new Array(indent + 1).join(' ') + ts.SyntaxKind[node.kind]);
-    indent++;
-    ts.forEachChild(node, printTree);
-    indent--;
+function printTree(node: ts.Node) {
+  console.log(new Array(indent + 1).join(" ") + ts.SyntaxKind[node.kind]);
+  indent++;
+  ts.forEachChild(node, printTree);
+  indent--;
 }
 printTree(sourceFile);
+
+/**
+ * Generates a nested data structure representing parent and their child nodes
+ * for a given node/ast.
+ *
+ * `[Kind, [Child, ...]]`
+ */
+export function generateTree(node: ts.Node, acc: string[][] = []) {
+  const parent: string = ts.SyntaxKind[node.kind];
+  const children: string[][] = [];
+
+  ts.forEachChild(node, (node: ts.Node) => {
+    generateTree(node, children);
+  });
+  acc.push([parent, children]);
+
+  return acc;
+}
+
+export function stringToSourceFile(code: string): ts.Node {
+  // 8 being ES2021, which sets the `languageVersion`.
+  return ts.createSourceFile("temp.ts", code, 8);
+}
+
+console.log(generateTree(sourceFile));
